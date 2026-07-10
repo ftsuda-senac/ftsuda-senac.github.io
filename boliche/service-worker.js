ae488dd707e827daef42ba28bd541927fb29ff04
ftsuda-senac <ftsuda.senac@gmail.com>
2026-07-09 22:48:34 -0300

    Ajustes visuais

--- boliche/service-worker.js
+++ boliche/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "boliche3d-v4";
+const CACHE_NAME = "boliche3d-v5";
 const ASSETS = [
   "./",
   "./index.html",
--- columns/service-worker.js
+++ columns/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "columns3d-v5";
+const CACHE_NAME = "columns3d-v6";
 const ASSETS = [
   "./",
   "./index.html",
